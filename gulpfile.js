1ba14a99627fd767310c33d0166e169a2efb434d
Adrián <arodriba@ull.edu.es>
2016-04-18 02:04:12 +0100

    Fixing and deleting unused things

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ gulp.task('minify-js', function() {
 });
 
 gulp.task('minify-css', function() {
-  return gulp.src(['public/*.css','public/vendor/*.css'])
+  return gulp.src(['public/*.css','public/*.css'])
     .pipe(cssmin({compatibility: 'ie8'}))
     .pipe(gulp.dest('minified'));
 })
